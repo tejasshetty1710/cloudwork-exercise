5c554b642a06101332276c74e6708f026e2b79c2
Tejas Shetty <tejasshetty1710@gmail.com>
2021-06-26 19:38:04 +0530

    add start work load

--- src/state/workloads/epics.ts
+++ src/state/workloads/epics.ts
@@ -1,12 +1,14 @@
 import { combineEpics, Epic } from 'redux-observable';
-import { filter, map, tap, ignoreElements } from 'rxjs/operators';
+import { filter, map, tap, ignoreElements, mergeMap, delay } from 'rxjs/operators';
 import { isActionOf } from 'typesafe-actions';
-
 import { RootAction, RootState } from '../reducer';
 import * as workloadsActions from './actions';
+import { from } from 'rxjs';
+import {WorkloadService} from './services';
 
 
 type AppEpic = Epic<RootAction, RootAction, RootState>;
+const WorkloadServiceInstance = new WorkloadService();
 
 const logWorkloadSubmissions: AppEpic = (action$, state$) => (
   action$.pipe(
@@ -17,9 +19,22 @@ const logWorkloadSubmissions: AppEpic = (action$, state$) => (
   )
 );
 
+const startWorkload: AppEpic = (action$, state$) => (
+	action$.pipe(
+    filter(isActionOf(workloadsActions.submit)),
+    mergeMap((action) =>
+			WorkloadServiceInstance.create({complexity: action.payload.complexity}).then(
+				(payload) => workloadsActions.created(payload)
+			)
+		)
+  )
+);
+
+
 
 export const epics = combineEpics(
   logWorkloadSubmissions,
+  startWorkload
 );
 
 export default epics;
